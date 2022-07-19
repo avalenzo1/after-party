fb8c1bacf4a3acf68b63a9d863648d8470bce3b5
Anthony Valenzo <avalenzo@cps.edu>
2022-07-19 17:18:50 -0500

    Initial Commit

---
+++ codenameChiventure/codenameChiventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  codenameChiventureApp.swift
+//  codenameChiventure
+//
+//  Created by Anthony Valenzo on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct codenameChiventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

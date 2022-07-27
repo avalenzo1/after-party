407f7672f598ff9666a9931b3b35f63de21fa0d8
Anthony Valenzo <avalenzo@cps.edu>
2022-07-26 21:44:42 -0500

    idk at this point 😭

--- codenameChiventure/HomeView.swift
+++ codenameChiventure/HomeView.swift
@@ -7,29 +7,48 @@
 
 import SwiftUI
 
+enum Flavor: String, CaseIterable, Identifiable {
+    case chocolate, vanilla, strawberry
+    var id: Self { self }
+}
+
 class HomeFormViewModel: ObservableObject {
     @Published var budget:Double = 0.0
-    @State var noOfPeople:Int = 1
+    @State var squadSize:Int = 0
     @State var category:String = ""
+    @State var selectedFlavor: Flavor = .chocolate
 }
 
 struct HomeView: View {
     @StateObject var viewModel = HomeFormViewModel()
     
     var body: some View {
-        VStack {
+        NavigationView {
             Section {
-                Text("Budget (TEMPORARY)")
+                // Budget Field
                 
-                TextField("Budget", value: $viewModel.budget, formatter: NumberFormatter())
+//                TextField("Budget", value: $viewModel.budget, formatter: NumberFormatter())
                 Slider(value: $viewModel.budget, in: 0.0...100.0, step: 1)
                     .accentColor(.red)
             }
             
             Section {
-                Text("How many People? (TEMPORARY)")
+                // People Field
                 
+                TextField("Total number of people", value: $viewModel.squadSize, formatter: NumberFormatter())
+                    .keyboardType(.numberPad)
+            }
+            
+            Section {
+                List {
+                    Picker("Flavor", selection: $viewModel.selectedFlavor) {
+                        Text("Chocolate").tag(Flavor.chocolate)
+                        Text("Vanilla").tag(Flavor.vanilla)
+                        Text("Strawberry").tag(Flavor.strawberry)
+                    }
+                }
             }
         }
+        .navigationTitle("Home")
     }
 }
